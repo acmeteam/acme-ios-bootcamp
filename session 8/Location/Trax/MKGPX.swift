39ea4ac2c09f92c66316b3867904109c5bc2fbef
Andrija Milovanović <andrija.milovanovic@endava.com>
2017-03-01 10:58:51 +0100

    Location services sample

---
+++ session 8/Location/Trax/MKGPX.swift
@@ -0,0 +1,49 @@
+
+import MapKit
+
+// EditableWaypoints are draggable
+// so their coordinate needs to be settable
+
+class EditableWaypoint : GPX.Waypoint
+{
+    override var coordinate: CLLocationCoordinate2D {
+        get {
+            return super.coordinate
+        }
+        set {
+            latitude = newValue.latitude
+            longitude = newValue.longitude
+        }
+    }
+}
+
+extension GPX.Waypoint : MKAnnotation
+{
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    var title: String? { return name }
+    
+    var subtitle: String? { return info }
+    
+    var thumbnailURL: URL? {
+        return getImageURLofType("thumbnail")
+    }
+    
+    var imageURL: URL? {
+        return getImageURLofType("large")
+    }
+    
+    // look in the hyperlink information from the GPX file
+    // try to find a url with a given type
+
+    fileprivate func getImageURLofType(_ type: String?) -> URL? {
+        for link in links {
+            if link.type == type {
+                return link.url as URL?
+            }
+        }
+        return nil
+    }
+}
